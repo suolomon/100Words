bb39f4dcd0d52c2658c9feddb83def6cc1f5b04c
Solomon Talemwa <adamtblack96@gmail.com>
2022-03-03 22:36:27 +0300

    feat: Add blog post component

---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,12 @@
+import Post from '../Post/Post'
+import './Posts.css'
+
+const Posts = () => {
+  return (
+    <div className="posts">
+        <Post />
+    </div>
+  )
+}
+
+export default Posts
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/Header/Header"
+import Posts from "../../components/Posts/Posts"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import "./Home.css"
 
@@ -7,6 +8,7 @@ const Home = () => {
     <>
         <Header/>
     <div className="home">
+      <Posts/>
       <Sidebar/>
     </div>
     </>
