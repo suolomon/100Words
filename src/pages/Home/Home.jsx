e0dc7d6376706124daf8321239ea8913642970b2
Solomon Talemwa <adamtblack96@gmail.com>
2022-03-16 21:36:53 +0300

    feat: Register user

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,17 +4,21 @@ import Header from "../../components/Header/Header";
 import Posts from "../../components/Posts/Posts";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import "./Home.css";
+import { useLocation } from "react-router-dom";
 
 const Home = () => {
   const [posts, setPosts] = useState([]);
+  const { search } = useLocation();
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get(`${import.meta.env.VITE_BLOG_URL}/posts`);
+      const res = await axios.get(
+        `${import.meta.env.VITE_BLOG_URL}/posts${search}`
+      );
       setPosts(res.data);
     };
     fetchPosts();
-  }, []);
+  }, [search]);
   return (
     <>
       <Header />
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,22 +1,70 @@
-import { Link } from 'react-router-dom'
-import './register.css'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./register.css";
+import axios from "axios";
 
-const register = () => {
+const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
+    setError(false)
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        `${import.meta.env.VITE_BLOG_URL}/auth/register`,
+        {
+          username,
+          email,
+          password,
+        }
+      );
+      res.data && window.location.replace("/login");
+    } catch (err) {
+      setError(true);
+    }
+  };
   return (
     <div className="register">
-        <span className="registerTitle">Register</span>
-        <form className="registerForm">
-            <label >Username</label>
-            <input type="text" placeholder="Enter Username" className="registerInput" />
-            <label >Email</label>
-            <input type="email" placeholder="Enter Email" className="registerInput" />
-            <label >Password</label>
-            <input type="password" placeholder="Enter Password" className="registerInput"/>
-            <button className="registerButton"><Link to="/register" className="link">Register</Link></button>
-            <button className="registerLoginButton"><Link to="/login" className="link">Login</Link></button>
-        </form>
+      <span className="registerTitle">Register</span>
+      <form className="registerForm" onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="text"
+          placeholder="Enter Username"
+          className="registerInput"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="Enter Email"
+          className="registerInput"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Enter Password"
+          className="registerInput"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="registerButton">
+          <Link to="/register" className="link">
+            Register
+          </Link>
+        </button>
+        <button className="registerLoginButton" type="submit">
+          <Link to="/login" className="link">
+            Login
+          </Link>
+        </button>
+      </form>
+        {error && <span style={{color: 'red', marginTop:'10px'}}>Something went wrong</span>}
     </div>
-  )
-}
+  );
+};
 
-export default register
+export default Register;
